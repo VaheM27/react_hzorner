711362ca3ac2253acd7e570208a9d4c1b57224ec
NGharibyan05 <narek.gharinyan.81@gmail.com>
2024-09-02 10:42:37 +0400

    lesson16

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,74 @@
-import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { object, string } from 'yup';
+import axios from 'axios';
+
+import './App.scss';
+import { nanoid } from 'nanoid';
 
 function App() {
+  const handleReg = (value) =>{
+    const newData = {
+      id:nanoid(4),
+      username: value.username,
+      email: value.email,
+      phonenumber: value.phonenumber,
+    };
+    axios.post('http://localhost:3000/posts',newData).then(()=>getData())
+    setData([...data,newData])
+  }
+  const [data,setData]= useState()
+
+  const getData = () =>{
+    axios('http://localhost:3000/posts').then(res=>setData(res.data))
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
+  const initialValues = {
+    username: '',
+    email: '',
+    phonenumber: '',
+  }
+
+  const validationSchema = object({
+    username: string().min(4,'minimum 4 character').max(15,'maximum 15 character').required(),
+    email: string().min(4,'minimum 4 character').max(15,'maximum 15 character').required(),
+    phonenumber: string().min(4,'minimum 4 character').max(15,'maximum 15 character').required(),
+  })
+
   return (
     <div className="App">
-      <h1>Narek's branch</h1>
+      <Formik
+        onSubmit={handleReg}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <Field type="text" name="username" placeholder="Username" />
+          <ErrorMessage component='p' name='username'/>
+          <Field type="email" name="email" placeholder="Email" />
+          <ErrorMessage  component='p' name='email'/>
+          <Field type="number" name="phonenumber" placeholder="Phone number" />
+          <ErrorMessage component='p' name='phonenumber'/>
+          <button type='submit'>Sign In</button>
+        </Form>
+      </Formik>
+
+      {
+        data.map((elm)=>{
+          return(
+            <div>
+              <p>{elm.username}</p>
+              <p>{elm.email}</p>
+              <p>{elm.phonenumber}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
