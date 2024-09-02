38bae78c00596f12bec63963ae319afb82afb49e
VaheM27 <vmnatsakanyan27@gmail.com>
2024-09-02 17:07:13 +0400

    Fixed map key

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,53 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { ErrorMessage, Field, Form, Formik } from 'formik';
-import axios from 'axios';
-import { nanoid } from 'nanoid';
-import { object, string } from 'yup';
+import { useState } from "react";
+import { useEffect } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import axios from "axios";
+import { nanoid } from "nanoid";
+import { object, string } from "yup";
 
-import './App.scss';
+import "./App.scss";
 
 function App() {
-  const handleReg = (value) =>{
+  const handleReg = (value) => {
     const newData = {
-      id:nanoid(4),
+      id: nanoid(4),
       username: value.username,
       email: value.email,
       phonenumber: value.phonenumber,
     };
-    axios.post('http://localhost:3000/posts',newData).then(()=>getData())
-    setData([...data,newData])
-  }
-  const [data,setData]= useState([])
+    axios.post("http://localhost:3000/posts", newData).then(() => getData());
+    setData([...data, newData]);
+  };
+  const [data, setData] = useState([]);
 
-  const getData = () =>{
-    axios('http://localhost:3000/posts').then(res=>setData(res.data))
-  }
+  const getData = () => {
+    axios("http://localhost:3000/posts").then((res) => setData(res.data));
+  };
 
-  useEffect(()=>{
-    getData()
-  },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
   const initialValues = {
-    username: '',
-    email: '',
-    phonenumber: '',
-  }
+    username: "",
+    email: "",
+    phonenumber: "",
+  };
 
   const validationSchema = object({
-    username: string().min(4,'minimum 4 character').max(15,'maximum 15 character').required(),
-    email: string().min(4,'minimum 4 character').max(15,'maximum 15 character').required(),
-    phonenumber: string().min(4,'minimum 4 character').max(15,'maximum 15 character').required(),
-  })
+    username: string()
+      .min(4, "minimum 4 character")
+      .max(15, "maximum 15 character")
+      .required(),
+    email: string()
+      .min(4, "minimum 4 character")
+      .max(15, "maximum 15 character")
+      .required(),
+    phonenumber: string()
+      .min(4, "minimum 4 character")
+      .max(15, "maximum 15 character")
+      .required(),
+  });
 
   return (
     <div className="App">
@@ -49,26 +58,24 @@ function App() {
       >
         <Form>
           <Field type="text" name="username" placeholder="Username" />
-          <ErrorMessage component='p' name='username'/>
+          <ErrorMessage component="p" name="username" />
           <Field type="email" name="email" placeholder="Email" />
-          <ErrorMessage  component='p' name='email'/>
+          <ErrorMessage component="p" name="email" />
           <Field type="number" name="phonenumber" placeholder="Phone number" />
-          <ErrorMessage component='p' name='phonenumber'/>
-          <button type='submit'>Sign In</button>
+          <ErrorMessage component="p" name="phonenumber" />
+          <button type="submit">Sign In</button>
         </Form>
       </Formik>
 
-      {
-        data.map((elm)=>{
-          return(
-            <div>
-              <p>{elm.username}</p>
-              <p>{elm.email}</p>
-              <p>{elm.phonenumber}</p>
-            </div>
-          )
-        })
-      }
+      {data.map((elm) => {
+        return (
+          <div key={elm.id}>
+            <p>{elm.username}</p>
+            <p>{elm.email}</p>
+            <p>{elm.phonenumber}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
