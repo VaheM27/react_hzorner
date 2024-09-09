1089b00b9f317353f833f64c13cb8c587841f011
VaheM27 <vmnatsakanyan27@gmail.com>
2024-09-09 17:06:26 +0400

    Background opacity

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,34 +1,30 @@
-import { useEffect } from 'react'
-import './Modal.scss'
+import { useEffect } from "react";
+import "./Modal.scss";
 
+const Modal = ({ handleDelete, deleteUser }) => {
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (e.target.className === "Modal") {
+        handleDelete();
+      }
+    };
 
-const Modal = ({handleDelete, deleteUser}) => {
+    window.addEventListener("click", handleClick);
 
-    useEffect(()=>{
+    return () => window.removeEventListener("click", handleClick);
+  }, []);
 
-        const handleClick = (e) =>{
-            if(e.target.className === 'Modal'){
-                handleDelete()
-            }
-        }
-
-        window.addEventListener('click', handleClick)
-
-        return () => window.removeEventListener('click', handleClick)
-
-    },[])
-
-
-    return (
-        <div className='Modal'>
-            <div className="ModalContent">
-                <p>Hastat uzum es jnjes es mardun?</p>
-                <div className="btn">
-                    <button onClick={deleteUser}>de vor sxmel em uzum em</button>
-                    <button onClick={handleDelete}>cheeee miamit em sxme</button>
-                </div>
-            </div>
+  return (
+    <div className="Modal">
+      <div className="ModalContent">
+        <p>Hastat uzum es jnjes es mardun?</p>
+        <div className="btn">
+          <button onClick={deleteUser}>de vor sxmel em uzum em</button>
+          <button onClick={handleDelete}>cheeee miamit em sxme</button>
         </div>
-    )
-}
-export default Modal
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/Lesson18/App.jsx
+++ src/Lesson18/App.jsx
@@ -1,62 +1,64 @@
-import { useEffect, useState } from 'react'
-import './App.scss'
-import axios from 'axios'
-import Modal from '../Components/Modal/Modal'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Modal from "../components/Modal/Modal"
 
 
-const App = () =>{
+import "./App.scss";
 
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [currentId, setCurrentId] = useState(null);
 
-    const [users,setUsers]= useState([])
-    const [modal,setModal] = useState(false)
-    const [currentId,setCurrentId] = useState(null)
-    
-    const handleDelete = () =>{
-        setModal(!modal)
-    }
+  const handleDelete = () => {
+    setModal(!modal);
+  };
 
-    const getCurrent = (id) =>{
-        setCurrentId(id)
-    }
+  const getCurrent = (id) => {
+    setCurrentId(id);
+  };
 
-    const deleteUser = () =>{
-        setUsers(users.filter((item)=> item.id != currentId))
-        handleDelete()
-        setCurrentId(null)
-    }
+  const deleteUser = () => {
+    setUsers(users.filter((item) => item.id !== currentId));
+    handleDelete();
+    setCurrentId(null);
+  };
 
-    useEffect(()=>{
-        axios({
-            baseURL:'https://jsonplaceholder.typicode.com/',
-            url:'users',
-            params:{
-                _limit: 9
-            }          
-        }).then(res=>setUsers(res.data))
-    },[])
+  useEffect(() => {
+    axios({
+      baseURL: "https://jsonplaceholder.typicode.com/",
+      url: "users",
+      params: {
+        _limit: 9,
+      },
+    }).then((res) => setUsers(res.data));
+  }, []);
 
+  return (
+    <div className="users">
+      {modal ? (
+        <Modal handleDelete={handleDelete} deleteUser={deleteUser} />
+      ) : null}
+      {users.map((item) => {
+        return (
+          <div key={item.id} className="userCard">
+            <p
+              onClick={() => {
+                handleDelete();
+                getCurrent(item.id);
+              }}
+            >
+              X
+            </p>
+            <p>{item.name}</p>
+            <p>{item.username}</p>
+            <p>{item.email}</p>
+            <p>{item.phone}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-    return (
-        <div className='users'>
-            {modal ? <Modal handleDelete={handleDelete} deleteUser = {deleteUser}/> : null}
-            {
-                users.map((item)=>{
-                    return(
-                        <div key={item.id} className='userCard'>
-                            <p onClick={()=>{
-                                handleDelete();
-                                getCurrent(item.id)
-                            }}>X</p>
-                            <p>{item.name}</p>
-                            <p>{item.username}</p>
-                            <p>{item.email}</p>
-                            <p>{item.phone}</p>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
-
-export default App
+export default App;
