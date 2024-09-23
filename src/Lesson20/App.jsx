400697a338b742dda3db55eb61005283e9c11ff4
VaheM27 <vmnatsakanyan27@gmail.com>
2024-09-23 16:57:54 +0400

    Changes

--- src/Lesson20/App.jsx
+++ src/Lesson20/App.jsx
@@ -1,27 +1,25 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom"
-import Nav from "./Nav/Nav"
-import Products from "./Products/Products"
-import ROUTES from "../Routes"
-import Home from "./Home/Home"
-import Detail from "./Detail/Detail"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./Nav/Nav";
+import Products from "./Products/Products";
+import ROUTES from "../Routes";
+import Home from "./Home/Home";
+import Detail from "./Detail/Detail";
 
-import './App.scss'
+import "./App.scss";
 
 const App = () => {
-    return (
-        <div className="App">
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.PRODUCTS} element={<Products />} />
+          <Route path="/products/:id" element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-            <BrowserRouter>
-                <Nav />
-                <Routes>
-                    <Route path={ROUTES.HOME} element={<Home />} />
-                    <Route path={ROUTES.PRODUCTS} element={<Products />} />
-                    <Route path="/products/:id" element={<Detail />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
-}
-
-
-export default App
+export default App;
--- src/Lesson20/Products/Products.jsx
+++ src/Lesson20/Products/Products.jsx
@@ -1,40 +1,35 @@
-import axios from 'axios'
-import './Products.scss'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
+import "./Products.scss";
 
+const Products = () => {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    axios("http://localhost:3000/products").then((res) => setData(res.data));
+  }, []);
 
+  const sorting = () => {
+    setData(data.toSorted((a, b) => a.price - b.price));
+  };
 
-const Products = () =>{
-    const [data,setData] = useState([])
-    
-    axios('http://localhost:3000/products').then(res=>setData(res.data))
+  return (
+    <div className="main">
+      <button onClick={sorting}>Sort</button>
+      {data.map((item) => {
+        return (
+          <div className="item" key={item.id}>
+            <p>{item.name}</p>
+            <img src={item.image} alt="" />
+            <p>{item.price}$</p>
+            <Link to={`/products/${item.id}`}>View Product</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-    const sorting = () =>{
-        const newData = data.sort((a,b)=>a.price - b.price)
-        setData(newData)
-    }
-
-
-    return(
-        <div className='main'>
-            <button onClick={sorting}>Sort</button>
-            {
-                data.map((item)=>{
-                    return(
-                        <div className='item' key={item.id}>
-                            <p>{item.name}</p>
-                            <img src={item.image} alt="" />
-                            <p>{item.price}$</p>
-                            <Link to={`/products/${item.id}`}>View Product</Link>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
-
-export default Products
+export default Products;
