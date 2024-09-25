3cbc1fb1b9fd430f7c8f07fa382b3c1a1d3613bc
NGharibyan05 <narek.gharinyan.81@gmail.com>
2024-09-25 12:46:06 +0400

    Slider

---
+++ src/Components/Slider/Sliders.jsx
@@ -0,0 +1,33 @@
+import Slider from "react-slick";
+
+import './Sliders.scss'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
+const Sliders = (images) =>{
+
+    console.log(images)
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      };
+          return (
+            <Slider {...settings}>
+              {
+                images.images.map((item)=>{
+                    return(
+                        <img src={item.src} alt="" />
+                    )
+                })
+              }
+            </Slider>
+          );
+}
+
+export default Sliders
