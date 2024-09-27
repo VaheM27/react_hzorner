ac5d032b0759c70885c01239c78a15a698d40198
VaheM27 <vmnatsakanyan27@gmail.com>
2024-09-27 16:56:49 +0400

    Done some code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import MarketPage from './Lesson22/MarketPage';
-import Product from "./Lesson22/components/Product/Product"
-import { useNumberList } from './Lesson24/useNumberList';
-import Number from './Lesson24/Number'
-
-
+import React from "react";
+import MarketPage from "./Lesson22/MarketPage";
+import Number from "./Lesson24/Number";
 
 const App = () => {
-
   return (
     <div className="App">
+      <MarketPage />
       <Number />
       <useNumberList />
-
     </div>
   );
 };
