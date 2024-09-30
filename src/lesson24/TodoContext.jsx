911c32e0906ad566b1f63fc016b51f2350b6ab15
VaheM27 <vmnatsakanyan27@gmail.com>
2024-09-30 17:08:28 +0400

    Fixed styles

--- src/lesson24/TodoContext.jsx
+++ src/lesson24/TodoContext.jsx
@@ -1,26 +1,32 @@
-import { nanoid } from 'nanoid';
-import React, { createContext, useState } from 'react';
-
+import { nanoid } from "nanoid";
+import React, { createContext, useState } from "react";
 
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
   const [todos, setTodos] = useState([
-    { id: 1, task: "Էս աբարանցին երազ ա տենում, մոտիկանում ա տենում ա երազ չի, գազել ա" },
-    { id: 2, task: 'Մութը ընգնում ա , աբարանցին մնում ա տակը' },
-    { id: 3, task: 'Ապարանցին ճակատը քորում ա,ճակատագիրը ջնջվում ա' },
-    { id: 4, task: 'Կառալյոկը մտնում ա Ապարան,սաղին խեղդում դուրս ա գալիս' },
-    { id:5, task: 'Ապարանցին երեխու անունը Արշակ ա ուզում դնի, երեխեն 7 ամսական ա ծնվում, անունը դնում ա Վաղարշակ' },
+    {
+      id: nanoid(10),
+      task: "Էս աբարանցին երազ ա տենում, մոտիկանում ա տենում ա երազ չի, գազել ա",
+    },
+    { id: nanoid(10), task: "Մութը ընգնում ա , աբարանցին մնում ա տակը" },
+    { id: nanoid(10), task: "Ապարանցին ճակատը քորում ա,ճակատագիրը ջնջվում ա" },
+    {
+      id: nanoid(10),
+      task: "Կառալյոկը մտնում ա Ապարան,սաղին խեղդում դուրս ա գալիս",
+    },
+    {
+      id: nanoid(10),
+      task: "Ապարանցին երեխու անունը Արշակ ա ուզում դնի, երեխեն 7 ամսական ա ծնվում, անունը դնում ա Վաղարշակ",
+    },
   ]);
 
-
   const addTodo = (task) => {
-    setTodos([...todos, { id: nanoid, task }]);
+    setTodos([...todos, { id: nanoid(10), task }]);
   };
 
-
   const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
